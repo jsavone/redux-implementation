fee885e3858a475b8e068911988e6265c7c2701e
Nisha Bharti <me.nishabharti@gmail.com>
2018-07-03 11:02:21 -0700

    created commented store.js file

---
+++ src/redux-COMPLETED/store.js
@@ -0,0 +1,11 @@
+//import createStore, Reducer
+import { createStore, combineReducers } from 'redux'
+import reducer from './reducer' //import reducer file
+
+const rootReducer = combineReducers({
+  items: reducer //this is an object passing aliases that takes keys and values..
+    //names how we will refer to the result, however Reducer is the action function.
+})
+
+//export createstore and teStore
+export default () => createStore(rootReducer)
