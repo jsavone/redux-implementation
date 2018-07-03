b82b4e4bf9e7f98146f22fb0972602903d98eb24
jsavone <john.savone@gmail.com>
2018-07-03 11:39:56 -0700

    fixed app.js and index.js issues

--- src/redux-COMPLETED/App.js
+++ src/redux-COMPLETED/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>this.props.items[0]</h1>
+      <h1>{this.props.items[0]}</h1>
       </div>
     );
   }
@@ -20,6 +20,6 @@ const mapStateToProps = state => {
   return {
     items: state.items
   }
-
+}
 //export default connect
 export default connect(mapStateToProps, null)(App);
--- src/redux-COMPLETED/index.js
+++ src/redux-COMPLETED/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './redux/reducer'
 import App from './App'
+import store from './redux/store'
 
-const store = createStore(rootReducer)
+let newStore = store();
 
 render(
-  <Provider store={store}>
+  <Provider store={newStore}>
     <App />
   </Provider>,
   document.getElementById('root')
