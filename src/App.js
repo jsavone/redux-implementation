e752a0c8f1d5e246b37ef69130353cdded47d2a0
jsavone <john.savone@gmail.com>
2018-07-03 10:24:19 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+// import connect from react-redux
+// import the action from actions
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Grab name from store here</h1>
+      </div>
+    );
+  }
+}
+
+//mapStatetoProps
+
+//export default connect
+export default App;
---
+++ src/redux/reducer.js
@@ -0,0 +1,5 @@
+// import the action
+
+//create variable for initial state
+
+//export state and action. Use switch s
