e0fa342934fed9912ca7434a09933377ed69d7c4
alphaborel <aaborel@gmail.com>
2018-07-03 11:00:04 -0700

    added comments to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import provider, createStore and the rootReducer
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//assign store to variable with createStore() method
+
+ReactDOM.render(
+  //wrap root lement into the service provider component
+  <App />, document.getElementById('root')
+
+);
 registerServiceWorker();
