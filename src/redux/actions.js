b5c4b276a6c142a6ced0e239c625776ba385c8ed
Unknown <erik.taylor30@gmail.com>
2018-07-03 10:48:58 -0700

    Auto stash before merge of "master" and "origin/master"
    add the action file

---
+++ src/redux/actions.js
@@ -0,0 +1,14 @@
+ // create a default variable
+ 
+ export const ADD_ITEM = 'ADD_ITEM'
+ 
+ 
+ // create the function 
+ export const = (list) =>{
+   //return an obj with the actions
+   return{
+     type:ADD_ITEM,
+     payload:item
+     
+   }
+ }
